aeb8976e826d3e900164195f62ba7d26419bfade
lambor <1973124271@qq.com>
2017-07-25 10:45:30 +0800

    scull

--- scull/scull.c
+++ scull/scull.c
@@ -5,16 +5,30 @@
 #include <linux/proc_fs.h>
 #include <linux/device.h>
 #include <asm/uaccess.h>
+#include <linux/slab.h>
+#include <linux/cdev.h>
+#include <linux/semaphore.h>
 
 static int scull_major = 0;
 static int scull_minor = 0;
 
+const int scull_quantum = 4000;
+const int scull_qset = 1000;
+const int scull_nr_devs = 4;
+
+struct scull_dev *scull_devices;
+
+ssize_t scull_read(struct file *filp, char __user *buf,size_t count, loff_t *offp);
+ssize_t scull_write(struct file *filp,const char __user *buf,size_t count,loff_t *f_pos);
+int scull_open(struct inode *inode,struct file *filp);
+int scull_release(struct inode *inode, struct file *filp);
+
 struct file_operations scull_fops = {
 	.owner = THIS_MODULE,
-	.llseek = scull_llseek,
+	//.llseek = scull_llseek,
 	.read = scull_read,
 	.write = scull_write,
-	.ioctl = scull_ioctl,
+	//.ioctl = scull_ioctl,
 	.open = scull_open,
 	.release = scull_release,
 };
@@ -22,7 +36,7 @@ struct file_operations scull_fops = {
 struct scull_qset {
 	void **data;
 	struct scull_qset *next;
-}
+};
 
 struct scull_dev {
 	struct scull_qset *data;
@@ -62,7 +76,7 @@ int scull_open(struct inode *inode,struct file *filp) {
 	dev = container_of(inode->i_cdev,struct scull_dev,cdev);
 	filp->private_data = dev;
 
-	if((filp->f_flag & O_ACCMODE) == O_WRONLY) {
+	if((filp->f_flags & O_ACCMODE) == O_WRONLY) {
 		scull_trim(dev);
 	}
 
@@ -73,11 +87,33 @@ int scull_release(struct inode *inode, struct file *filp) {
 	return 0;
 }
 
-ssize_t read(struct file *filp, char __user *buf,size_t count, loff_t *offp) {
+static struct scull_qset *scull_follow(struct scull_dev *dev,int n) {
+	struct scull_qset *qs = dev->data;
+
+	if(!qs) {
+		qs = dev->data = kmalloc(sizeof(struct scull_qset), GFP_KERNEL);
+		if(qs == NULL) return NULL;
+		memset(qs,0,sizeof(struct scull_qset));
+	}
+
+	while(n--) {
+		if(!qs->next) {
+			qs->next = kmalloc(sizeof(struct scull_qset), GFP_KERNEL);
+			if(qs->next == NULL) return NULL;
+			memset(qs->next,0,sizeof(struct scull_qset));
+		}
+		qs = qs->next;
+		continue;
+	}
+	return qs;
+}
+
+ssize_t scull_read(struct file *filp, char __user *buf,size_t count, loff_t *f_pos) {
 	struct scull_dev *dev = filp->private_data;
 	struct scull_qset *dptr;
 	int quantum = dev->quantum, qset = dev->qset;
 	int itemsize = quantum * qset;
+	int item, s_pos, q_pos, rest;
 	ssize_t retval = 0;
 
 	if(down_interruptible(&dev->sem)) return -ERESTARTSYS;
@@ -98,6 +134,7 @@ ssize_t read(struct file *filp, char __user *buf,size_t count, loff_t *offp) {
 		retval = -EFAULT;
 		goto out;
 	}
+	
 	*f_pos += count;
 	retval = count;
 
@@ -109,7 +146,7 @@ out:
 ssize_t scull_write(struct file *filp,const char __user *buf,size_t count,loff_t *f_pos) {
 	struct scull_dev *dev = filp->private_data;
 	struct scull_qset *dptr;
-	int quantum quantum * qset;
+	int quantum = dev->quantum, qset = dev->qset;
 	int itemsize = quantum * qset;
 	int item,s_pos,q_pos,rest;
 	ssize_t retval = -ENOMEM;
@@ -121,6 +158,7 @@ ssize_t scull_write(struct file *filp,const char __user *buf,size_t count,loff_t
 	s_pos = rest / quantum; q_pos = rest % quantum;
 
 	dptr = scull_follow(dev,item);
+
 	if(dptr == NULL) goto out;
 	if(!dptr->data) {
 		dptr->data = kmalloc(qset * sizeof(char *),GFP_KERNEL);
@@ -149,14 +187,22 @@ out:
 	return retval;
 }
 
-ssize_t write(struct file *filp, const char __user *buf,size_t count,loff_t *offp) {
+static void scull_setup_dev(struct scull_dev *dev, int index) {
+	int err, devno = MKDEV(scull_major,scull_minor+index);
 
+	cdev_init(&dev->cdev,&scull_fops);
+	dev->cdev.owner = THIS_MODULE;
+	dev->cdev.ops = &scull_fops;
+	err = cdev_add(&dev->cdev,devno,1);
+	if(err)
+		printk(KERN_ALERT"Error %d adding scull%d",err,index);
 }
 
 static int __init scull_init(void) 
 {
 	int err = -1;
 	dev_t dev = 0;
+	int i;	
 	
 	printk(KERN_ALERT"Initializing scull device.\n");
 
@@ -165,19 +211,48 @@ static int __init scull_init(void)
 	if(err < 0)
 	{
 		printk(KERN_ALERT"Failed to alloc freg device.\n");
+		goto fail;
 	}
 
 	scull_major = MAJOR(dev);
 	scull_minor = MINOR(dev);
 
+
+	scull_devices = kmalloc(scull_nr_devs*sizeof(struct scull_dev),GFP_KERNEL);
+	if(!scull_devices)
+	{
+		err = -ENOMEM;
+		goto free_chrdev;
+	}
+	memset(scull_devices,0,scull_nr_devs*sizeof(struct scull_dev));
+
+	for(i=0;i<scull_nr_devs;i++) {
+		scull_devices[i].quantum = scull_quantum;
+		scull_devices[i].qset = scull_qset;
+		sema_init(&scull_devices[i].sem,1);
+		scull_setup_dev(&scull_devices[i],i);
+	}
+
+	return 0;
+
+free_chrdev:
+	unregister_chrdev_region(MKDEV(scull_major,scull_minor),4);
+fail:
 	return err;
 }
 
 static void __exit scull_exit(void)
 {
+	int i;
+	dev_t devno = MKDEV(scull_major,scull_minor);
 	printk(KERN_ALERT"Destroy scull device.\n");
 
-	dev_t devno = MKDEV(scull_major,scull_minor);
+	for(i=0;i<scull_nr_devs;i++) {
+		scull_trim(scull_devices+i);
+		cdev_del(&scull_devices[i].cdev);	
+	}
+	kfree(scull_devices);
+
 	unregister_chrdev_region(devno,4);
 }
 
